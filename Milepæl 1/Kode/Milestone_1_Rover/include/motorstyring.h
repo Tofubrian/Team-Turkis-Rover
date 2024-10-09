2a63bff08916d96efec82aaf66f5013f7d64e808
ElonMusk42O <mrha67055@edu.ucl.dk>
2024-10-09 10:30:15 +0200

    Ændring af struktur

--- Milepæl 1/Kode/Milestone_1_Rover/include/motorstyring.h
+++ Milepæl 1/Kode/Milestone_1_Rover/include/motorstyring.h
@@ -9,39 +9,40 @@
 
 int incomingByte = 0; // for incoming serial data
 
-void backward() {          //function of forward 
-  analogWrite(A1, 255);
-  analogWrite(A2, 0);
-  analogWrite(B1, 255);
-  analogWrite(B2, 0);
-}
-
-void forward() {         //function of backward
+void forward() {           // Kør Forlæns
   analogWrite(A1, 0);
   analogWrite(A2, 255);
   analogWrite(B1, 0);
   analogWrite(B2, 255);
 }
 
-// turn right loop
-void turnRight() {
-  analogWrite(A1, 225);
+void backward() {          // Kør Baglens
+  analogWrite(A1, 255);
+  analogWrite(A2, 0);
+  analogWrite(B1, 255);
+  analogWrite(B2, 0);
+}
+
+void Stop() {              // Stop
+  analogWrite(A1, 0);
+  analogWrite(A2, 0);
+  analogWrite(B1, 0);
+  analogWrite(B2, 0);
+}
+
+void turnRight() {       // Drej til højre
+  analogWrite(A1, 225);  
   analogWrite(A2, 0);
   analogWrite(B1, 0);
   analogWrite(B2, 225);
 }
 
-// turn left loop
-void turnLeft() {
+void turnLeft() {        // Drej til venstre
   analogWrite(A1, 0);
   analogWrite(A2, 255);
   analogWrite(B1, 225);
   analogWrite(B2, 0);
 }
-
-void Stop() {
-  analogWrite(A1, 0);
-  analogWrite(A2, 0);
-  analogWrite(B1, 0);
-  analogWrite(B2, 0);
-}
+/*Grunden til at turn har 225 i stedet for 255 er pga. 
+for at kunne dreje et antal grader præcist, måtte 
+hastigheden sættes ned for at undgå ustabillitet og hjulspin.*/
