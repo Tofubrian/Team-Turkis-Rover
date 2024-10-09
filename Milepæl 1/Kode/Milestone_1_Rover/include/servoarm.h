e147cb47f4f51bc346a8bbeb21b47eda1778c7c4
Thorbjorn <mrdcp1991@gmail.com>
2024-10-09 11:01:29 +0200

    kode tilføjet til servoarm

--- Milepæl 1/Kode/Milestone_1_Rover/include/servoarm.h
+++ Milepæl 1/Kode/Milestone_1_Rover/include/servoarm.h
@@ -2,8 +2,38 @@
 #include <Wire.h>
 #include <VL53L0X.h> 
 
-//position for servoarm
-int armpos() {
 
+#define PIN 
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin();
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  // tell the servo class which pin we are using
+  servo.attach(SERVO_PIN);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // set up a message handler for the 'servo' feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  servo_feed->onMessage(handleMessage);
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
 
 }
