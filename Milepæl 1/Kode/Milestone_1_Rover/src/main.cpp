9ab9e3e2322dbf2414248845d7884c6fcc379a01
ElonMusk42O <mrha67055@edu.ucl.dk>
2024-10-09 09:56:34 +0200

    Kommentaret tilføjet i Main kode

--- Milepæl 1/Kode/Milestone_1_Rover/src/main.cpp
+++ Milepæl 1/Kode/Milestone_1_Rover/src/main.cpp
@@ -10,8 +10,9 @@
 #include <VL53L0X.h>
 #include <motorstyring.h> 
 
+// Dette er tilsluttet på vores ESP32
 #define A1 25  // Motor A pins
-#define A2 26
+#define A2 26 // Thorbjørn sagde jeg skulle lave en kommentar
 #define B1 14 // Motor B pins
 #define B2 12
 
