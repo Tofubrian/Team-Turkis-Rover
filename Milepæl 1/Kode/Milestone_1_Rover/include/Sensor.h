cde3deca8b2c58256f4ff2cba0b70edad81fc556
StoreA <andersfoens@gmail.com>
2024-10-09 10:38:44 +0200

    Adding skeleton functions for sensor

--- Milepæl 1/Kode/Milestone_1_Rover/include/Sensor.h
+++ Milepæl 1/Kode/Milestone_1_Rover/include/Sensor.h
@@ -1,4 +1,23 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <VL53L0X.h> 
+#include <VL53L0X.h>
+#include <motorstyring.h>
 
+// Functions for the sensors goes in here
+
+// Function for moving forward, right and left
+// do while loop instead maybe? 
+void sensorAuto () {
+    if (sensor > 50 && speed < 255) {
+        turnRight();
+    }
+    else if () {
+        turnLeft();
+
+    }
+    else {
+        forward();
+        
+        
+    }
+}
