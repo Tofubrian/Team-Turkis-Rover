39f822c0041ea9a57ff164b29d54fd214fc4667a
StoreA <andersfoens@gmail.com>
2024-10-09 09:29:44 +0200

    Inserted header file for turn on and off LED

---
+++ Milepæl 1/Kode/Milestone_1_Rover/include/led.h
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+
+// Define pins here pretty pleaaase
+// Change to the correct number
+// Change pin variable name to what we build
+#define LED1 12
+
+// Copy the below code to define new pins
+
+// #define LEDx 11
+
+
+// Initialize pins so they are ready to use in code.
+// Example below
+void setup () {
+    pinMode(redLED, OUTPUT);
+    
+    // Copy the below code to initialize another led
+
+    // pinMode(LEDx, OUTPUT);
+}
+
+void LEDTurnOn () {
+    digitalWrite(LED1, HIGH);
+
+}
+
+void LEDTurnOff () {
+    digitalWrite(LED1, LOW);
+
+}
