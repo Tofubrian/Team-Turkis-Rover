57cdca4ef8fb392978a62d40c4be66072d7fd88f
Toki3500 <tvwk67039@edu.ucl.dk>
2024-11-13 10:06:35 +0100

    Arm 90 grader rotationer

--- Milepæl 1/Kode/Milestone_1_Rover/include/servoarm.h
+++ Milepæl 1/Kode/Milestone_1_Rover/include/servoarm.h
@@ -55,7 +55,7 @@ public:
     // Function to pinch (close the grab servo)
     void pinch() {
         // Close the grab servo (move from 180 to 0 degrees)
-        for (int i = 180; i > 0; i -= 3) {
+        for (int i = 90; i > 0; i -= 2) {
             servoGrab.write(i);
             delay(20);  // Slightly longer delay to allow the servo to move
         }
@@ -64,49 +64,49 @@ public:
     // Function to open the grab servo
     void open() {
         // Open the grab servo (move from 0 to 180 degrees)
-        for (int i = 0; i < 180; i += 3) {
+        for (int i = 0; i < 90; i += 2) {
             servoGrab.write(i);
             delay(20);  // Slightly longer delay
         }
     }
     void BottomLeft() {
         // Open the grab servo (move from 0 to 180 degrees)
-        for (int i = 0; i < 180; i += 4) {
+        for (int i = 0; i < 90; i += 2) {
             servoBottom.write(i);
             delay(20);  // Slightly longer delay
         }
     }
     void BottomRight() {
         // Open the grab servo (move from 0 to 180 degrees)
-        for (int i = 180; i > 0; i -= 4) {
+        for (int i = 90; i > 0; i -= 2) {
             servoBottom.write(i);
             delay(20);  // Slightly longer delay
         }
     }
     void ArmForward() {
         // Open the grab servo (move from 0 to 180 degrees)
-        for (int i = 180; i > 0; i -= 2) {
+        for (int i = 90; i > 0; i -= 2) {
             servoRight.write(i);
             delay(20);  // Slightly longer delay
         }
     }
     void ArmBackward() {
         // Open the grab servo (move from 0 to 180 degrees)
-        for (int i = 0; i < 180; i += 2) {
+        for (int i = 0; i < 90; i += 2) {
             servoRight.write(i);
             delay(20);  // Slightly longer delay
         }
     }
      void ArmUp() {
         // Open the grab servo (move from 0 to 180 degrees)
-        for (int i = 180; i > 0; i -= 3) {
+        for (int i = 90; i > 0; i -= 2) {
             servoLeft.write(i);
             delay(20);  // Slightly longer delay
         }
     }
     void ArmDown() {
         // Open the grab servo (move from 0 to 180 degrees)
-        for (int i = 0; i < 130; i += 2) {
+        for (int i = 0; i < 90; i += 2) {
             servoLeft.write(i);
             delay(20);  // Slightly longer delay
         }
