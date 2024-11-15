7b01f4689782666eb8d419c72955f6e89573aa87
StoreA <andersfoens@gmail.com>
2024-11-15 12:18:38 +0100

    Working joystick manual drive

--- Milepæl 1/Kode/Milestone_1_Rover/include/joystick_drive.h
+++ Milepæl 1/Kode/Milestone_1_Rover/include/joystick_drive.h
@@ -5,11 +5,11 @@
 // ************************* START JOYSTICK ************************* //
 
 // Joystick definition and variables here
-const int ManualpinX = 39;  // Joystick X-axis pin
-const int ManualpinY = 36;  // Joystick Y-axis pin
+const int ManualpinX = 35;  // Joystick X-axis pin
+const int ManualpinY = 34;  // Joystick Y-axis pin
 
 // ******* LOCKED VALUES FOR JOYSTICK ******* // 
-const int joystick_center = 512; // Center of the joystick // 2048
+const int joystick_center = 520; // Center of the joystick // 2048
 const int joystick_threshold = 50; // Dead zone threshold // 200
 const int joystick_max_value = 1023; // Maximum reading for x,y axis
 const int joystick_min_value = 462; // Minimum reading for x,y axis
@@ -29,48 +29,93 @@ MotorController motors(25, 26, 14, 12);  // A1, A2, B1, B2
 // ************************* JOYSTICK DRIVE MODE ************************* //
 
 void manualMode() {
-    int joyPinX = analogRead(ManualpinX); // Read X-axis value  
-    int joyPinY = analogRead(ManualpinY); // Read X-axis value  
+    int inputX = analogRead(ManualpinX); // Read X-axis value  
+    int inputY = analogRead(ManualpinY); // Read X-axis value  
 
-    if (joyPinY > joystick_center + joystick_threshold) {
-        motors.forward();
-        Serial.println("Going forward");
+    int joyPinX = inputX / 4;
+    int joyPinY = inputY / 4;
+
+    Serial.println("X value is ");
+    Serial.println(joyPinX);
+
+    Serial.println("Y value is ");
+    Serial.println(joyPinY);
+
+    if (joyPinY > joystick_center + joystick_threshold) { // Forward
+        if (joyPinX > joystick_center + joystick_threshold) {
+            motors.turnSmoothLeft(); // Forward Left
+            Serial.println("Turning forward left");
+        }
+        else if (joyPinX < joystick_center - joystick_threshold) {
+            motors.turnSmoothRight(); // Forward Right
+            Serial.println("Turning forward right");
+        }
+        else {
+            motors.forward(); // Straight Forward
+            Serial.println("Going forward");
+        }
     }
-    else if (joyPinY > joystick_center + joystick_threshold && joyPinX > joystick_center + joystick_threshold) {
+    else if (joyPinY < joystick_center - joystick_threshold) { // Backward
+        if (joyPinX > joystick_center + joystick_threshold) {
+            motors.backwardSmoothLeft(); // Backward Left
+            Serial.println("Backwards left");
+        }
+        else if (joyPinX < joystick_center - joystick_threshold) {
+            motors.backwardSmoothRight(); // Backward Right
+            Serial.println("Backwards right");
+        }
+        else {
+            motors.backward(); // Straight Backward
+            Serial.println("Going backward");
+        }
+    }
+    else if (joyPinX > joystick_center + joystick_threshold) { // Idle movement Right
         motors.turnSmoothLeft();
-        Serial.println("Turning forward left");
+        Serial.println("Turning idle left");
     }
-    else if (joyPinY > joystick_center + joystick_threshold && joyPinX > joystick_center - joystick_threshold) {
+    else if (joyPinX < joystick_center - joystick_threshold) { // Idle movement Left
         motors.turnSmoothRight();
-        Serial.println("Turning forward right");
+        Serial.println("Turning idle right");
     }
-    else {
-        motors.stop();
-        Serial.println("Idle mode");
-    }
-    
-    // Delay here for smooth movements with the joystick
-    delay(100);
-
-    if (joyPinY < joystick_center - joystick_threshold) {
-        motors.backward();
-        Serial.println("Going backward");
-    }
-    else if (joyPinY < joystick_center - joystick_threshold && joyPinX > joystick_center + joystick_threshold){
-        motors.backwardSmoothLeft();
-        Serial.println("Backwards left");
-    }
-    else if (joyPinY < joystick_center - joystick_threshold && joyPinX < joystick_center - joystick_threshold) {
-        motors.backwardSmoothRight();
-        Serial.println("Backwards right");
-    }
-    else {
+    else { // Idle
         motors.stop();
         Serial.println("Idle mode");
     }
 
     // Delay here for smooth movements with the joystick
     delay(100);
+
+    // if (joyPinY > joystick_center + joystick_threshold) {
+    //     motors.forward();
+    //     Serial.println("Going forward");
+    // }
+    // else if (joyPinY > joystick_center + joystick_threshold && joyPinX > joystick_center + joystick_threshold) {
+    //     motors.turnSmoothLeft();
+    //     Serial.println("Turning forward left");
+    // }
+    // else if (joyPinY > joystick_center + joystick_threshold && joyPinX > joystick_center - joystick_threshold) {
+    //     motors.turnSmoothRight();
+    //     Serial.println("Turning forward right");
+    // }
+    // else if (joyPinY < joystick_center - joystick_threshold) {
+    //     motors.backward();
+    //     Serial.println("Going backward");
+    // }
+    // else if (joyPinY < joystick_center - joystick_threshold && joyPinX > joystick_center + joystick_threshold){
+    //     motors.backwardSmoothLeft();
+    //     Serial.println("Backwards left");
+    // }
+    // else if (joyPinY < joystick_center - joystick_threshold && joyPinX < joystick_center - joystick_threshold) {
+    //     motors.backwardSmoothRight();
+    //     Serial.println("Backwards right");
+    // }
+    // else {
+    //     motors.stop();
+    //     Serial.println("Idle mode");
+    // }
+
+    // // Delay here for smooth movements with the joystick
+    // delay(100);
 }
 
 // ************************* JOYSTICK END DRIVE MODE ************************* //
