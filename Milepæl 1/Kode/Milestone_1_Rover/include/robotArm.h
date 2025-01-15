7f3eb3d6e4eaae68a7a60e079a6fe86726b30548
Toki3500 <tvwk67039@edu.ucl.dk>
2025-01-15 14:47:54 +0100

    madsrover+armdone

--- Milepæl 1/Kode/Milestone_1_Rover/include/robotArm.h
+++ Milepæl 1/Kode/Milestone_1_Rover/include/robotArm.h
@@ -24,12 +24,12 @@ int currentGrabPosition = 120;
 // Constants for servo angles
 const int BASE_MIN_ANGLE = 30;
 const int BASE_MAX_ANGLE = 150;
-const int JOINT1_MIN_ANGLE = 30; //80 orginal værdier
+const int JOINT1_MIN_ANGLE = 30; //80 orginale værdier
 const int JOINT1_MAX_ANGLE = 180; //150
 const int JOINT2_MIN_ANGLE = 30; //70
 const int JOINT2_MAX_ANGLE = 180; //120
-const int GRAB_MIN_ANGLE = 80;
-const int GRAB_MAX_ANGLE = 160;
+const int GRAB_MIN_ANGLE = 70; //80
+const int GRAB_MAX_ANGLE = 160;  //160
 
 // Movement speed
 const int SERVO_STEP_SIZE = 2;  // Increment/decrement step size for movement
